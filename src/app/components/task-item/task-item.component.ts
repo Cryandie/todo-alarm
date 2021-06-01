208d4bd3ad0098a7ed6b0330fa5cdc5144a20a9c
Seif BS <seifbensghaier@gmail.com>
2021-06-01 21:57:26 +0100

    1st commit

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit , Input , Output , EventEmitter} from '@angular/core';
+import { Task } from './../../Task';
+
+
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task! : Task ;
+  @Output() onDeleteEvent: EventEmitter<Task> = new EventEmitter();
+  @Output() toggleReminderEvent: EventEmitter<Task> = new EventEmitter();
+
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
+
+onDeleteClick(task:Task) {
+ this.onDeleteEvent.emit(task);
+}
+toggleReminderClick(task:Task){
+  this.toggleReminderEvent.emit(task);
+}
+
+}
